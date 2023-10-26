93cf08d292d5b47c944b714be4132d36da52328b
denkovalsky <artyom.noskov.1998@yandex.ru>
2023-10-27 04:02:33 +0500

    Основной сервис.

--- stats-service/server/src/main/resources/schema.sql
+++ stats-service/server/src/main/resources/schema.sql
@@ -1,8 +1,9 @@
-CREATE TABLE IF NOT EXISTS STATS (
+CREATE TABLE IF NOT EXISTS STATS
+(
     id      BIGINT GENERATED ALWAYS AS IDENTITY NOT NULL,
-    app     VARCHAR(255) NOT NULL,
-    uri     VARCHAR(255) NOT NULL,
-    ip      VARCHAR(255) NOT NULL,
+    app     VARCHAR(255)                        NOT NULL,
+    uri     VARCHAR(255)                        NOT NULL,
+    ip      VARCHAR(255)                        NOT NULL,
     created TIMESTAMP WITHOUT TIME ZONE,
     CONSTRAINT pk_stat PRIMARY KEY (id)
 );
